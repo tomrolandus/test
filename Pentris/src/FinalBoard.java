450521b53fb75b62b57f717f405a7fb25b679a9e
tomrolandus <tomrolandus@gmail.com>
2014-11-03 17:20:33 +0100

    i've been working on the final class

--- Pentris/src/FinalBoard.java
+++ Pentris/src/FinalBoard.java
@@ -1,31 +1,41 @@
-
 public class FinalBoard {
 
-	public FinalBoard() {
-	}
+	private char[][] grid;
 
-	public FinalBoard(int width, int height) {
-		// TODO Auto-generated constructor stub
+	public FinalBoard(int height, int width) {
+		grid = new char[height][width];
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				grid[row][col] = 0;
+			}
+		}
 	}
 
 	public void removeLine(int row) {
-		// TODO Auto-generated method stub
-		
+		for (int rw = row; rw >= 0; rw--)
+			for (int cl = 0; cl < grid[rw].length; cl++)
+				if (rw == 0)
+					grid[rw][cl] = 0;
+				else
+					grid[rw][cl] = grid[rw - 1][cl];
+
 	}
 
 	public boolean checkFullRow(int row) {
-		// TODO Auto-generated method stub
-		return false;
+		for(int c = 0; c < grid[row].length; c++)
+			if(grid[row][c]==0)
+				return false;
+	return true;
 	}
 
 	public boolean checkFloorCollision(Pentomino pent, int[] location) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	public void putPentomino(Pentomino pent, int[] location) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public boolean checkHitCeiling() {
