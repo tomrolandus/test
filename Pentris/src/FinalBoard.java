3aa376ec791a1473a425a97d23deb73ce5e33a96
tomrolandus <tomrolandus@gmail.com>
2014-11-04 11:42:40 +0100

    all my methods are made and commented except for checkFloorCollision because I don't know how to get it to work with position[].

--- Pentris/src/FinalBoard.java
+++ Pentris/src/FinalBoard.java
@@ -1,7 +1,11 @@
 public class FinalBoard {
 
 	private char[][] grid;
-
+	/**
+	 * Initialize the finalBoard and fill it with zeros.
+	 * @param height the height of the board
+	 * @param width the width of the board
+	 */
 	public FinalBoard(int height, int width) {
 		grid = new char[height][width];
 		for (int row = 0; row < height; row++) {
@@ -10,7 +14,10 @@ public class FinalBoard {
 			}
 		}
 	}
-
+	/**
+	 * This method will remove a row
+	 * @param row gives the full row that has to be removed
+	 */
 	public void removeLine(int row) {
 		for (int rw = row; rw >= 0; rw--)
 			for (int cl = 0; cl < grid[rw].length; cl++)
@@ -20,26 +27,58 @@ public class FinalBoard {
 					grid[rw][cl] = grid[rw - 1][cl];
 
 	}
-
+	/**
+	 * Checks if a given row is full or not.
+	 * @param row the row to check for fullness
+	 * @return False if the row is not full and TRUE if the row is full.
+	 */
 	public boolean checkFullRow(int row) {
 		for(int c = 0; c < grid[row].length; c++)
 			if(grid[row][c]==0)
 				return false;
 	return true;
 	}
-
-	public boolean checkFloorCollision(Pentomino pent, int[] location) {
-		
+	/**
+	 * This method checks if the pentomino that is sliding down collides with something.
+	 * @param pent
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean checkFloorCollision(Pentomino pent, int row, int col) {
+		int[][] pentom = pent.getShape();
+		for(int i = 0; i < pentom.length; i++)
+			for(int j = 0; j < pentom[i].length; j++)
+				if(pentom[i][j] == 1 && grid[row-1][j] == )
+					//not done yet
+				
+					
+					
+				
 		return false;
 	}
-
-	public void putPentomino(Pentomino pent, int[] location) {
-		// TODO Auto-generated method stub
+	/**
+	 * This method places the pentomino on the finalBoard
+	 * @param pent The pentomino that has to be placed.
+	 * @param row The row of the pentomino that has to be placed.
+	 * @param col The column of the pentomino that has to be placed.
+	 */
+	public void putPentomino(Pentomino pent, int row, int col)
+	{
+		int[][] pento = pent.getShape();
+		for(int i = 0; i<pento.length; i++)
+			for(int j = 0; i<pento[i].length; j++)
+				if(pento[i][j] == 1)
+				grid[row+i][col+j] = pent.getType();
 
 	}
-
+	/**
+	 * Checks if the top of the board has been reached by a pentomino.
+	 * @return TRUE if the top has been reached. False if the top has not yet been reached.
+	 */
 	public boolean checkHitCeiling() {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < grid.length; i++)
+			if(grid[0][i] != 0) return true;
 		return false;
 	}
 
