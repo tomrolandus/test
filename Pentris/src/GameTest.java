a02baba1358bec2c5e9c6b5e2396a2928b6f6aab
tomrolandus <tomrolandus@gmail.com>
2014-11-06 12:07:54 +0100

    test unit

---
+++ Pentris/src/GameTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author tomrolandus
+ *
+ */
+public class GameTest {
+
+	@Test
+	public void test() {
+		Game game = new Game();
+		game.start();
+		System.out.println(game.getScore());
+		
+		
+	}
+
+}
